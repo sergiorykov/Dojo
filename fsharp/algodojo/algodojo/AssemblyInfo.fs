d5e1262d85362be2bc86f481c4bfc5c29af9de80
Sergio Rykov <sergior@bk.ru>
2017-03-11 01:44:58 +0300

    added algodojo with sample module

---
+++ fsharp/algodojo/algodojo/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace algodojo.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("algodojo")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("algodojo")>]
+[<assembly: AssemblyCopyright("Copyright ©  2017")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("e1823bde-23c5-4a95-b21b-63c77822e543")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ fsharp/algodojo/tests/HelloWorldTests.fs
@@ -0,0 +1,9 @@
+﻿namespace tests
+
+open Xunit
+open FsUnit
+
+type ``Hello World tests`` ()=
+    [<Fact>] member test.
+     ``Should always succeed`` ()=
+        Choice<int,int>.Choice1Of2(1) |> should be (choice 1)
